48413f0c942ec47b7d06b125016486860a8f6817
Lester Lie <waakye@Lesters-Mac-mini.local>
2021-09-16 20:57:05 +0800

    Next commit

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require('express');
 
 const app = express();
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end('<h1>Hello World</h1>');
-// });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
