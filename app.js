770e552f2c83db3ebd46bc76cb29993613851094
Lester Lie <waakye@Lesters-Mac-mini.local>
2021-09-16 20:54:27 +0800

    Add route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+const app = express();
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
 });
 
 server.listen(port, () => {
